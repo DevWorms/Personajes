876cf3d9801c9c1bbc676695877aa8edd6a7aeff
koupper <koupperrn@live.com>
2017-06-13 18:56:48 -0500

    prefab de bagman listo

---
+++ Bagman/Assets/Scripts/BagmanMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BagmanMovement : MonoBehaviour {
+
+    public Animator anim;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            
+        if (Input.GetKey("d"))
+        {
+            anim.SetBool("caminandoDer", true);
+            anim.SetBool("isIddle", false);
+            anim.SetBool("jumping", false);
+            anim.SetBool("isAtacking", false);
+        }
+        else if (Input.GetKey("w"))
+        {
+            anim.SetBool("jumping", true);
+            anim.SetBool("caminandoDer", false);
+            anim.SetBool("isIddle", false);
+            anim.SetBool("isAtacking", false);
+        }
+        else if (Input.GetKey("space"))
+        {
+            anim.SetBool("jumping", false);
+            anim.SetBool("caminandoDer", false);
+            anim.SetBool("isIddle", false);
+            anim.SetBool("isAtacking", true);
+        }
+        else
+        {
+            anim.SetBool("caminandoDer", false);
+            anim.SetBool("isIddle", true);
+            anim.SetBool("jumping", false);
+            anim.SetBool("isAtacking", false);
+        }
+        
+    }
+}
