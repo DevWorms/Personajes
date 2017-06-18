7376a4b568ca83baded47744d4ffe1f23520bd63
koupper <koupperrn@live.com>
2017-06-18 04:12:31 -0500

    bagman semi-completo(aun esta mejorable)

--- Bagman/Assets/Scripts/BagmanMovement.cs
+++ Bagman/Assets/Scripts/BagmanMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class BagmanMovement : MonoBehaviour {
 
     public Animator anim;
+    public Transform bagMan;
+    public float velocidad;
     // Use this for initialization
     void Start()
     {
@@ -14,35 +16,48 @@ public class BagmanMovement : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        
-            
-        if (Input.GetKey("d"))
-        {
-            anim.SetBool("caminandoDer", true);
-            anim.SetBool("isIddle", false);
-            anim.SetBool("jumping", false);
-            anim.SetBool("isAtacking", false);
-        }
-        else if (Input.GetKey("w"))
+        if (Input.GetKey("w"))
         {
             anim.SetBool("jumping", true);
             anim.SetBool("caminandoDer", false);
             anim.SetBool("isIddle", false);
             anim.SetBool("isAtacking", false);
-        }
-        else if (Input.GetKey("space"))
-        {
-            anim.SetBool("jumping", false);
-            anim.SetBool("caminandoDer", false);
-            anim.SetBool("isIddle", false);
-            anim.SetBool("isAtacking", true);
+            bagMan.Translate((velocidad * new Vector2(0, 3)) * Time.deltaTime);
+
         }
         else
-        {
-            anim.SetBool("caminandoDer", false);
-            anim.SetBool("isIddle", true);
-            anim.SetBool("jumping", false);
-            anim.SetBool("isAtacking", false);
+        { 
+
+            if (Input.GetKey("d"))
+            {
+                anim.SetBool("caminandoDer", true);
+                anim.SetBool("isIddle", false);
+                anim.SetBool("jumping", false);
+                anim.SetBool("isAtacking", false);
+                bagMan.Translate((velocidad * new Vector2(2, 0)) * Time.deltaTime);
+            }
+            else if (Input.GetKey("a"))
+            {
+                anim.SetBool("jumping", false);
+                anim.SetBool("caminandoDer", false);
+                anim.SetBool("isIddle", true);
+                anim.SetBool("isAtacking", false);
+                bagMan.Translate((velocidad * new Vector2(-2, 0)) * Time.deltaTime);
+            }
+            else if (Input.GetKey("space"))
+            {
+                anim.SetBool("jumping", false);
+                anim.SetBool("caminandoDer", false);
+                anim.SetBool("isIddle", false);
+                anim.SetBool("isAtacking", true);
+            }
+            else
+            {
+                anim.SetBool("caminandoDer", false);
+                anim.SetBool("isIddle", true);
+                anim.SetBool("jumping", false);
+                anim.SetBool("isAtacking", false);
+            }
         }
         
     }
