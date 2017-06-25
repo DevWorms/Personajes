6d573cf3a9ade8f156146178828cb13271f1ab68
Jh0nBorg <darthjmg2003@hotmail.com>
2017-06-25 00:51:06 -0500

    Enemigo papel cuaderno incluye menu...
    incluye

---
+++ paper_animation/Assets/playerScreen/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    public Texture2D btnMenu;
+    public Texture2D btnMenuRollOver;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnGUI()
+    {
+        
+    }
+}
