894a4dfa5a5b69b315e55c7a729872ceb6caa476
Unknown <koupperrn@live.com>
2017-06-29 23:06:44 +0800

    nivel 1

---
+++ BM Personajes/Assets/Scripts/camaraMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class camaraMovement : MonoBehaviour
+{
+    public GameObject spartan;
+   
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position =    new Vector3(spartan.transform.position.x+4.03f, spartan.transform.position.y+2.2f,-10f);
+
+
+
+    }
+}
+
